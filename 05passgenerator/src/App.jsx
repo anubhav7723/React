6f2a1766ef2486dcb172b92bc622aee35dd0d317
Anubhav Gupta <anubhavg0098@gmail.com>
2025-03-03 20:17:16 +0530

    add some files

---
+++ 05passgenerator/src/App.jsx
@@ -0,0 +1,61 @@
+import {useCallback, useEffect, useRef, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [length,setlength]=useState(5);
+  const [numAllow,setnumAllow]=useState(false);
+  const [spChar,setspChar]=useState(false);
+  const [password,setPassword]=useState("");
+  const passGenRef=useRef(null)
+
+  const passGen = useCallback(()=>{
+    let pass="";
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (numAllow) str += "0123456789"
+    if (spChar) str += "~!@#$%^&*()-_=+[{]}|"
+
+    for(let i=1;i<=length;i++){
+      let char= Math.floor(Math.random()*str.length+1)
+      pass +=str.charAt(char)
+    }
+    setPassword(pass)
+  },[length,numAllow,spChar,setPassword])
+
+  const copypasswordToclipboard=useCallback(()=>{
+    passGenRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  useEffect(()=>{
+    passGen()
+  },[length,numAllow,spChar,passGen])
+
+  return (
+    <>
+      <div className='text-black bg-pink-200 w-full max-w-md my-8 mx-auto shadow-md py-3 rounded-lg px-4 '>
+        <h1 className='text-center text-black py-1 mb-5'>PassWord Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden '>
+          <input type="text" value={password} readOnly className='w-full  py-1 px-3 outline-none bg-white text-black rounded-b-sm'ref={passGenRef} placeholder='password'/>
+          <button className='bg-blue-800 text-white px-3  shrink-0 outline-none' onClick={copypasswordToclipboard}>Copy</button>
+        </div>
+        <div className='flex text-sm gap-x-3 mt-5'>
+          <div className='flex items-center gap-x-1'>
+            <input type="range" min={6} max={20} value={length} className='cursor-pointer' onChange={(e)=>{setlength(e.target.value)}}/>
+            <label>Length: {length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox" defaultChecked={numAllow} id="numberInput" onChange={()=>{setnumAllow((prev)=>!prev)}} />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox" defaultChecked={spChar} id="charInput" onChange={()=>{setspChar((prev)=>!prev)}} />
+            <label htmlFor="numberInput"> Sp. Characters</label>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setcolor] = useState("black")
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 bg-white h-14 rounded-xl left-80">
+          <div className="flex flex-wrap  justify-center gap-3 shadow-xl px-2 py-1 h-12 ">
+            <button onClick={()=>setcolor("red")} className="outline-none px-6 py-3 rounded-2xl shadow-sm text-white hover:cursor-pointer"
+            style={{ backgroundColor: 'red' }}>Red</button>
+            <button onClick={()=>setcolor("blue")} className="outline-none px-6 py-3 rounded-2xl shadow-sm text-white hover:cursor-pointer"
+            style={{ backgroundColor: 'blue' }}>Blue</button>
+            <button onClick={()=>setcolor("green")} className="outline-none px-6 py-3 rounded-2xl shadow-sm text-white hover:cursor-pointer"
+            style={{ backgroundColor: 'green' }}>Green</button>
+            <button onClick={()=>setcolor("black")} className="outline-none px-6 py-3 rounded-2xl shadow-sm text-white hover:cursor-pointer"
+            style={{ backgroundColor: 'black' }}>Black</button>
+            <button onClick={()=>setcolor("yellow")} className="outline-none px-6 py-3 rounded-2xl shadow-sm text-black hover:cursor-pointer"
+            style={{ backgroundColor: 'yellow' }}>Yellow</button>
+            <button onClick={()=>setcolor("pink")} className="outline-none px-6 py-3 rounded-2xl shadow-sm text-black hover:cursor-pointer"
+            style={{ backgroundColor: 'pink' }}>Pink</button>
+            <button onClick={()=>setcolor("grey")} className="outline-none px-6 py-3 rounded-2xl shadow-sm text-black hover:cursor-pointer"
+            style={{ backgroundColor: 'grey' }}>Grey</button>
+            <button onClick={()=>setcolor("olive")} className="outline-none px-6 py-3 rounded-2xl shadow-sm text-white hover:cursor-pointer"
+            style={{ backgroundColor: 'olive' }}>Olive</button>
+            <button onClick={()=>setcolor("brown")} className="outline-none px-6 py-3 rounded-2xl shadow-sm text-white hover:cursor-pointer"
+            style={{ backgroundColor: 'brown' }}>Brown</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
